1b779a9f031829d4c1c67f198026636389fdb602
Rubika <mrubika92@gmail.com>
2021-02-21 22:13:18 +0530

    ignore sentences not suitable for QA generation

--- FrontEnd/processFile.py
+++ FrontEnd/processFile.py
@@ -1,89 +1,85 @@
 from rippletagger.tagger import Tagger
 import rulebased
 import snowballstemmer
+from polyglot.text import Text
+
+dictionary = {
+    'location': ['ரஷ்யா', 'பிரான்ஸ்', 'ஆஸ்திரியா-ஹங்கேரி', 'பல்கேரியா', 'ஐக்கிய அமெரிக்கா',
+                 'பிரித்தானியா', 'இத்தாலி', 'ஓட்டோமான்', 'ஜெர்மன்', 'ரோமானியா', 'ஜப்பான்'],
+
+    'person': ['நிக்கோலாஸ் II', 'அலெக்சேய் புருசிலோவ்', 'ஜார்ஜஸ் கிளெமென்சியு', 'ஜோசப் ஜோப்ரே', 'பேர்டினண்ட் ஃபோக்',
+               'ராபர்ட் நிவேலே','பிலிப் பெட்டேன்','ஹெர்பேர்ட் எச். அஸ்குயித்', 'டே. லாயிட் ஜார்ஜ்', 'டக்ளஸ் ஹேக்',
+               'ஜான் ஜெலிக்கோ', 'விக்டர் இம்மானுவேல் III', 'லுய்கி கடோர்னா', 'ஆர்மண்டோ டயஸ்', 'வூட்ரோ வில்சன்',
+               'ஜான் பேர்ஷிங்', 'வின்ஸ்டன் சர்ச்சில்', 'பிராங்க்ளின் டெலானோ ரூஸ்வெல்ட்', 'பேரரசர் ஹிரோஹிட்டோ',
+               'அடோல்ஃப் ஹிட்லர்', 'பெனிட்டோ முசோலினி', 'ஜோசப் ஸ்டாலின்', 'டக்ளஸ் மாக்ஆர்தர்', 'டுவைட் ஐசனோவர்',
+               'மன்னர் ஜார்ஜ் V']
+}
+
+def lookup_search(lookup):
+    for key, value in dictionary.items():
+        for v in value:
+            if lookup in v:
+                return key
+    return 'None'
+
+def checkanophoricresolution(sentence, firstword, stemword):
+    personrefs = ['அவர்', 'அவருக்கு', 'அவரை', 'அவரின்', 'அவரது', 'அவரால்', 'இவர்', 'இவரை', 'இவரது', 'இவரின்',
+                  'இவருக்கு', 'அவரால்']
+    locationrefs = ['அங்கே', 'இங்கே', 'அங்கு', 'இங்கு']
+
+    return True
+
+def ignorenonsuitablesentence(sentence):
+    ignorewordlistfile = open("ignoresentence.txt", encoding="utf-8")
+    ignorewordlist = ignorewordlistfile.read().splitlines()
+
+    firstword = sentence.partition(' ')[0]
+    if firstword in ignorewordlist:
+        return False
+    stemmer = snowballstemmer.stemmer('tamil')
+    stemWord = stemmer.stemWords(firstword.split())
+    if stemWord and stemWord[0] in ignorewordlist:
+        return False
+
+    return checkanophoricresolution(sentence,firstword, stemWord)
 
 def processfile(filecontent, filewritepath):
     stopwordsfile = open("stopwords.txt", encoding="utf-8")
     stopwords = stopwordsfile.read().splitlines()
-    print(stopwords)
 
     writefile = open(filewritepath, "w", encoding="utf-8")
     sentences = filecontent.split(".")
-    print(sentences)
+
     for sentence in sentences:
-        matchFound = rulebased.regex_match_multiple_items(sentence, writefile)
-        if matchFound:
+        sentence = ' '.join(sentence.split())
+        issentencesuitable = ignorenonsuitablesentence(sentence)
+        if not issentencesuitable:
             continue
+
+        tagger = Tagger(language='tam')
+        postagger = tagger.tag(sentence)
+        print(sentence)
+        print('POS tag', postagger)
 
         matchFound = rulebased.regex_match_date_time_quantity(sentence, writefile)
         if matchFound:
             continue
 
-        tagger = Tagger(language='tam')
-        posTagger = tagger.tag(sentence)
-        print(sentence)
-        print('POS tag', posTagger)
-        for (word, tag) in posTagger:
+        matchFound = rulebased.regex_match_multiple_items(sentence, writefile)
+        if matchFound:
+            continue
+
+        for (word, tag) in postagger:
+
             if tag == 'NOUN' and word not in stopwords:
-                stemmer = snowballstemmer.stemmer('tamil')
-                stemWord = stemmer.stemWords(word.split())
-                print(stemWord)
 
-
-#import PyPDF2
-#import textract
-#import tamil
-#import transliterate
-#from rippletagger.tagger import Tagger
-#INDIC_NLP_RESOURCES=r"C:\Users\ASUS\Desktop\MSC\Research Seminar\indic_nlp_resources-master"
-
-#text = tamil.txt2unicode.auto2unicode("©Ûuß uÚx øPPÍõÀ ö£õ¸ÒPøÍ EØ£zv ö\´ÁuØS¨ £v»õP C¯¢vµ[PøÍ¨ £¯ß£kzv ö£õ¸Ò EØ£zvø¯ ÷©ØöPõshxhß _©õº BÖ u\õ¨uPõ»¨£Sv°À øPzöuõÈÀ ©ØÖ® öuõÈÝm£z xøÓ°À HØ£kzv¯ £õ›¯Ó÷© øPzö Ø ¦µm] GßÖ AøÇUP¨£kQÓx. CøÁ öuõhº£õP C¨ £õhzvÀ UP¨£mkÒÍx. ")
-#print(text)
-#Export the path to the Indic NLP Resources directory programmatically
-
-#from indicnlp import common
-#common.set_resources_path(INDIC_NLP_RESOURCES)
-
-#Initialize the Indic NLP library
-#from indicnlp import loader
-#loader.load()
-
-#from indicnlp.tokenize import indic_tokenize
-#indic_string='अनूप,अनूप?।फोन'
-#print('Input String: {}'.format(indic_string))
-#print('Tokens: ')
-#for t in indic_tokenize.trivial_tokenize(indic_string):
-    #print(t)
-
-
-#file = open("Sample doc tscii.txt", errors='ignore')
-#text = ""
-#for line in file:
- #   line= line.rstrip()
- #   if line:
-  #      text= text + line
-
-#text1 = tamil.tscii.convert_to_unicode("Áµ»õÖ Gß£x CÓ¢u Põ»zvÀ ÁõÌ¢u ©Ûu ö\¯Ø£õkPÒ £ØÔU PØS® £õh©õS®. £À»õ°µ® Á¸h[PÐUS •ßÚµõÚ Áµ »õØøÓU PØS®÷£õx AUPõ»Pmh[PÎÀ AÁºPÒ GÊv øÁzu  h¯[PÐ® AÁºPÎß £À÷ÁÖ BUP[PÐ® QøhUP¨ ö£ÖQßÓÚ. A¢u GÊzuõÁn[PÒ AÀ»x BUP[PÒ ‰»® •ØPõ» ©Ûu ö\¯Ø£õkPÒ £ØÔ¯ uPÁÀPøÍ¨ ö£ØÖUöPõÒÍ •iÁuÚõÀ AøÁ ‰»õuõµ[PÒ GÚ¨£kQßÓÚ. JÆöÁõ¸ Põ»PmhzvØS® E›¯uõPU QøhUP¨ö£Ö® CÆÁõÓõÚ ‰»õuõµ[PÎß ‰»® AUPõ»a \‰Pa `ÇÀ £ØÔ AÔ¢x öPõÒÍ •i²®. CÆÁõÓõÚ ‰»õuõµ[PÎß ‰»® ö£ØÖUöPõÒÐ® uPÁÀPøÍz uºUP Ÿv¯õPU PØS®÷£õx AÆÁU Põ»¨ £SvUPõÚ ©Ûu ÁõÌUøP £ØÔ¯ Áµ»õÖ Pmiö¯Ê¨£¨£kQßÓx. Áµ»õØøÓ PØS®÷£õx uPÁÀPøÍ¨ ö£ØÖU öPõÒÍUTi¯ ‰»õuõµ[PÒ AvPÍ À Põn¨£mh ÷£õv¾® AÁØøÓ Cµsk ÁøP PÍõP¨ ¤›zx ÷|õUP»õ®.")
-#print(text1)
-
-#x = text.split(".")
-#for t in x:
- #tagger = Tagger(language='tam')
- #posTagger = tagger.tag(t)
- #print('POS tag', posTagger)
- #if posTagger.contains('NOUN'):
- #   print(posTagger, 'with noun')
-
-
-# text = Text('புத்துணர்ச்சியான')
-# text.language = "ta"
-# print(text.morphemes)
-
-
-
-#text = tamil.txt2unicode.auto2unicode("©Ûuß uÚx øPPÍõÀ ö£õ¸ÒPøÍ EØ£zv ö\´ÁuØS¨ £v»õP C¯¢vµ[PøÍ¨ £¯ß£kzv ö£õ¸Ò EØ£zvø¯ ÷©ØöPõshxhß _©õº BÖ u\õ¨uPõ»¨£Sv°À øPzöuõÈÀ ©ØÖ® öuõÈÝm£z xøÓ°À HØ£kzv¯ £õ›¯Ó÷© øPzö Ø ¦µm] GßÖ AøÇUP¨£kQÓx. CøÁ öuõhº£õP C¨ £õhzvÀ UP¨£mkÒÍx. ")
-#print(text)
-
-#text = tamil.txt2unicode.tam2unicode("©Ûuß uÚx øPPÍõÀ ö£õ¸ÒPøÍ EØ£zv ö\´ÁuØS¨ £v»õP C¯¢vµ[PøÍ¨ £¯ß£kzv ö£õ¸Ò EØ£zvø¯ ÷©ØöPõshxhß _©õº BÖ u\õ¨uPõ»¨£Sv°À øPzöuõÈÀ ©ØÖ® öuõÈÝm£z xøÓ°À HØ£kzv¯ £õ›¯Ó÷© øPzö Ø ¦µm] GßÖ AøÇUP¨£kQÓx. CøÁ öuõhº£õP C¨ £õhzvÀ UP¨£mkÒÍx. ")
-#print(text)
+                matchFound = lookup_search(word)
+                if matchFound == 'location':
+                    question = sentence.replace(word, "எந்த நாட்டில்")
+                    rulebased.writeqafile(writefile, question, word)
+                    break
+                elif matchFound == 'person':
+                    question = sentence.replace(word, "யார்")
+                    rulebased.writeqafile(writefile, question, word)
+                    break
 
--- FrontEnd/rulebased.py
+++ FrontEnd/rulebased.py
@@ -1,58 +1,43 @@
 import re
 
+def writeqafile(writefile, question, answer):
+    writefile.write(question + "?\n")
+    print(answer)
+    writefile.write(answer + "\r\n")
+    return True
+
 #regex match for எந்த, எவை question generation
 def regex_match_multiple_items(sentence, writefile):
     multipleItemsRegex1 = re.compile('(.*,\s)+.*(?:ஆகிய\s|போன்ற\s)')
     match = re.match(multipleItemsRegex1, sentence)
     if match:
         question = re.sub(multipleItemsRegex1, 'எந்த ', sentence)
-        print('question: ' + question + '?')
-        writefile.write(question + "?\n")
-        print(match.string)
-        writefile.write(match.string + "\r\n")
-        return True
+        return writeqafile(writefile, question, match.string)
     else:
-        multipleItemsRegex2 = re.compile('(.*,\s)+.*(?:ஆகியன\s|என்பன\s|போன்றன\s|போன்றவை\s|ஆகியவை\s|என்பவை\s)')
+        multipleItemsRegex2 = re.compile('(.*,)+.*(?:ஆகியன\s|என்பன\s|போன்றன\s|போன்றவை\s|ஆகியவை\s|என்பவை\s)')
         match2 = re.match(multipleItemsRegex1, sentence)
         if match2:
             question = re.sub(multipleItemsRegex2, 'எவை ', sentence)
-            print('question2: ' + question + '?')
-            writefile.write(question + "?\n")
-            print(match.string)
-            writefile.write(match.string + "\r\n")
-            return True
+            return writeqafile(writefile, question, match.string)
 
-#regex match for எப்போது, எத்தனை question generation
+#regex match for எப்போது, எத்தனை type of question generation
 def regex_match_date_time_quantity(sentence, writefile):
-    dateTimeRegex = re.compile('(கி\.பி\s)?[1-2][0-9]{3}\s?(?:இல்|ல்)')
-    match = re.match(dateTimeRegex, sentence)
-    print("line 21")
+    dateTimeRegex = re.compile('(வருடம்\s)*[1-2][0-9]{3}\s*(?:இல்\s|ல்\s)')
+    match = re.search(dateTimeRegex, sentence)
     if match:
         question = re.sub(dateTimeRegex, 'எந்த வருடம் ', sentence)
-        print('question: ' + question + '?')
-        writefile.write(question + "?\n")
-        print(match.string)
-        writefile.write(match.string + "\r\n")
-        return True
+        return writeqafile(writefile, question, match.string)
     else:
-        dateTimeRegex2 = re.compile('[1-2][0-9]{3}\s?(?:ஆம்)')
-        match2 = re.match(dateTimeRegex2, sentence)
-        print("line 29")
+        dateTimeRegex2 = re.compile('[1-2][0-9]{3}(\s)*(?:ஆம்)')
+        match2 = re.search(dateTimeRegex2, sentence)
         if match2:
             question = re.sub(dateTimeRegex2, 'எந்த', sentence)
-            print('question2: ' + question + '?')
-            writefile.write(question + "?\n")
-            print(match.string)
-            writefile.write(match.string + "\r\n")
-            return True
+            return writeqafile(writefile, question, match2.string)
         else:
             dateTimeRegex3 = re.compile('\d{1,2}\s(?:ஆம் நூற்றாண்)')
-            match3 = re.match(dateTimeRegex3, sentence)
-            print("line 37")
+            match3 = re.search(dateTimeRegex3, sentence)
             if match3:
                 question = re.sub(dateTimeRegex3, 'எத்தனையாம் நூற்றாண்', sentence)
-                print('question3: ' + question + '?')
-                writefile.write(question + "?\n")
-                print(match.string)
-                writefile.write(match.string + "\r\n")
-                return True
+                return writeqafile(writefile, question, match3.string)
+
+    return False
